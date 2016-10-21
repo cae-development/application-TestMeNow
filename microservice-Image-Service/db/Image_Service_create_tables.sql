d56bc083d6fc8ca56db74ba7af3d647ec2373c81
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-10-21 18:15:08 +0200

    Added microservice Image Service

---
+++ microservice-Image-Service/db/Image_Service_create_tables.sql
@@ -0,0 +1,14 @@
+--
+-- Database Schema:  cae-schema 
+-- Automatically generated sql script for the service Image Service, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table images.
+--
+CREATE TABLE `cae-schema`.`images` (
+  url varchar(255) ,
+  id int NOT NULL AUTO_INCREMENT ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+);
+
